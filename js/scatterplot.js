3cea37c993c2260037f2f90fa26a6f8bfdc95cc9
kschmidt2 <k.schmidt2@gmail.com>
2016-10-29 20:40:18 -0400

    adds scatterplot and sets up the page

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -3,13 +3,13 @@ var buildScatter = function(selectState) {
   console.log(selectState);
 
   // margins
-  var margin = {top: 20, right: 80, bottom: 290, left: 100},
-      width = 865 - margin.left - margin.right,
-      height = 670 - margin.top - margin.bottom;
+  var margin = {top: 30, right: 250, bottom: 130, left: 150},
+      width = 900 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
 
 
   // create responsive svg
-  var svg = d3.select("#canvas1")
+  var svg = d3.select("#scattercanvas")
       .append("div")
       .classed("svg-container-line " + selectState + "-svg", true) //container class to make it responsive
       .append("svg")
@@ -21,6 +21,15 @@ var buildScatter = function(selectState) {
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var x = d3.scaleLinear()
+        .range([0,width]);
+
+    var y = d3.scaleLinear()
+        .range([height,0]);
+
+
+  // var designation = ["Main", "Branch"];
+  // var designation_color = ["#47b4f2","#b0c422"];
 
   // load data
   var institutionFile = "data/institutional-data.csv";
@@ -58,21 +67,72 @@ var buildScatter = function(selectState) {
   var plot_data = function(data) {
       console.log(data);
 
-      svg.selectAll("text")
-          .data(data)
-        .enter().append("text")
-          .text(selectState);
+      x.domain([0, d3.max(data, function(d) { return d.median_earnings; })]).nice();
+      y.domain([0, d3.max(data, function(d) { return d.mean_debt_graudated;  })]).nice();
+
+      svg.append("g")
+          .attr("class", "axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x))
+        .selectAll("text")
+          .attr("y", 5)
+      .attr("x", -37)
+          .attr("transform", "rotate(-45)")
+          .style("text-anchor", "start")
+        .append("text")
+          .attr("class", "label")
+          .attr("x", width)
+          .attr("y", -6)
+          .style("text-anchor", "end")
+          .text("Median earnings");
+
+      svg.append("g")
+          .attr("class", "axis")
+          .attr("transform", "translate(0,0)")
+          .call(d3.axisLeft(y))
+        .append("text")
+          .attr("class", "label")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Average debt")
+
+          svg.selectAll(".dot")
+            .data(data)
+          .enter().append("circle")
+            .attr("class", function(d) { return "dot " + d.campus_type })
+            .attr("r", 3.5)
+            .attr("cx", function(d) { return x(d.median_earnings); })
+            .attr("cy", function(d) { return y(d.mean_debt_graudated); })
+            .attr("r", function(d) { if (d.completion_rate === 0) { return 5 } else { return (d.completion_rate)*20}});
+
+      // svg.selectAll("text")
+      //     .data(data)
+      //   .enter().append("text")
+      //     .text(function (d) { return d.median_earnings });
 
   }
 
 
+
+
 };
 
-$(".state").one('click', function(){
+$(".state")
+.on('click', function(){
   $this = this;
   var stateClass = $(this).attr('id');
+  var enterState = '<div class="hidden-xs sf sf-' + stateClass.toLowerCase() + '"></div><h2>' + stateClass + '</h2>';
+  $('.scatter').html('<div id="scattercanvas"></div>');
+  $('#schoolinfo').html(enterState);
   buildScatter(stateClass);
-})
-.on('click', function(){
-  $('#canvas1').fadeIn(500);
+  $('.bottom-row').addClass('bottom-border');
+
+
+// })
+// .one('click', function(){
+//   $this = this;
+//   var stateClass = $(this).attr('id');
+//   buildScatter(stateClass);
 });
