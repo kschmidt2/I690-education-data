3190019619b5d5b54add4810f5f4f2d01612e4bb
Katherine Shaw <shaw.comma.katherine@gmail.com>
2016-11-15 13:42:39 -0500

    clean up old code/comments

--- js/lineChart.js
+++ js/lineChart.js
@@ -1,78 +1,78 @@
- /*This function creates the line chart that helps us to compare the amount of state funding per student in the selected state*/
- function buildLineChart(selectState, state_data){
+/*This function creates the line chart that helps us to compare the amount of state funding per student in the selected state*/
+function buildLineChart(selectState, state_data) {
+    //Defines the size of the various attributes in the visualization canvas
+    var height = 500;
+    var width = 500;
+    var margin = 40;
+    var margin_left = 40;
 
-        //Defines the size of the various attributes in the visualization canvas
-        var height = 500;
-        var width = 500;
-        var margin = 40;
-        var margin_left = 40;
+    // Create the SVG canvas that will be used to render the visualization.
+    var svg = d3v4.select("#vis_container")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-        // Create the SVG canvas that will be used to render the visualization.
-        var svg = d3v4.select("#vis_container")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
+    // Define the axis scales and formats
+    var x = d3v4.scaleLinear()
+        .domain([1998, 2016])
+        .range([margin, width - margin]);
 
+    var xAxis = d3v4.axisBottom(x)
+        .tickFormat(d3v4.format(".0f"));
+        //this formats the ticks on the axis and removes the , from the thousands
 
-        // Defines the scales
-        var x = d3v4.scaleLinear()
-            .domain([ 1998, 2016])
-            .range([margin, width - margin]);
+    var y = d3v4.scaleLinear()
+        .domain([15000 , 5000])
+        .range([margin, height - margin]);
 
-        var xAxis = d3v4.axisBottom(x).tickFormat(d3v4.format(".0f"));//this formats the ticks on the axis and removes the , from the thousands
+    // Add x axis to graph
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0,"+(500 - margin)+")")
+        .call(xAxis);
 
-        // Life expectancy values all fall between 70 and 90.
-        var y = d3v4.scaleLinear()
-            .domain([ 15000 , 5000])
-            .range([margin, height - margin]);
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("y", 450)
+        .attr("x",0 + (440))
+        .style("text-anchor", "middle")
+        .text("Year");
 
-         // Add axes.  First the X axis and label.
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0,"+(500 - margin)+")")
-            .call(xAxis);
+    // Add y axis to graph
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin_left + ",0)")
+        .call(d3v4.axisLeft(y));
 
-        svg.append("text")
-            .attr("class", "axis-label")
-            .attr("y", 450)
-            .attr("x",0 + (440))
-            .style("text-anchor", "middle")
-            .text("Year");
-
-        // Now the Y axis and label.
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + margin_left + ",0)")
-            .call(d3v4.axisLeft(y));
-
-        //Make changes here to change the way Average funding is written on the chart
-        svg.append("text")
-            .attr("transform", "rotate(90)")
-            .attr("class", "axis-label")
-            .attr("y", - 50)
-            .attr("x", 0 + (80))
-            .style("text-anchor", "middle")
-            .text("Average funding");
+    //Make changes here to change the way Average funding is written on the chart
+    svg.append("text")
+        .attr("transform", "rotate(90)")
+        .attr("class", "axis-label")
+        .attr("y", - 50)
+        .attr("x", 0 + (80))
+        .style("text-anchor", "middle")
+        .text("Average funding");
 
 
-        // Now a clipping plain for the main axes
-        // Add the clip path.
-        svg.append("clipPath")
-            .attr("id", "clip")
-            .append("rect")
-                  .attr("x", margin)
-                  .attr("y", margin)
-                  .attr("width", width - 2 * margin)
-                  .attr("height", height - 2 * margin);
+    // Now a clipping plain for the main axes
+    // Add the clip path.
+    // Are we going to animate transitions? If not, we can probably remove this.
+    svg.append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("x", margin)
+        .attr("y", margin)
+        .attr("width", width - 2 * margin)
+        .attr("height", height - 2 * margin);
 
-        //Loading the data
-        console.log(state_data[selectState]);
 
-        var lineFunc = d3v4.line()
-            .x(function (d) { return x(d.year); })
-            .y(function (d) { return y(d.state_funding / d.ft_students); });
+    // Add data
+    var lineFunc = d3v4.line()
+        .x(function (d) { return x(d.year); })
+        .y(function (d) { return y(d.state_funding / d.ft_students); });
 
-        svg.append("svg:path")
+    // console.log(state_data[selectState]);
+    svg.append("svg:path")
             .attr("d", lineFunc(state_data[selectState]))
             .attr("stroke", "red")
             .attr("stroke-width",3)
