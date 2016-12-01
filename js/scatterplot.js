4879a8b5f4e4a2adc43ab96c974c9c23f8e43350
kschmidt2 <k.schmidt2@gmail.com>
2016-12-01 14:48:09 -0500

    correcting merge issue

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -87,8 +87,8 @@ var buildScatterplot = function(selectState) {
         .attr("cy", function(d) { return y(d.mean_debt_graduated); })
         .attr("stroke", "#fff")
         .attr("fill", "#d2d9da")
-        // .on("mouseover", scatterHover)
-        .on("click", buildBarCharts);
+        .on("click", buildBarCharts)
+        .on("mouseover", buildBarCharts);
 
     // creates dropdown menu for each state
     var dropDown = d3.select("#school-selector").append("select")
@@ -115,162 +115,147 @@ var buildScatterplot = function(selectState) {
     function buildBarCharts(selectedSchool) {
 
         var bar_groups = [
-          {"name": "mean earnings", "values": [
+          {"name": "Median earnings", "values": [
               {"category": "school", "amount": selectedSchool.median_earnings},
               {"category": "state", "amount": d3.mean(filtered_data,function(d) { return d.median_earnings; })},
               {"category": "national", "amount": national_avgs.median_earnings}
           ]},
-          {"name": "mean debt", "values": [
+          {"name": "Average debt", "values": [
                 {"category": "school", "amount": selectedSchool.mean_debt_graduated},
                 {"category": "state", "amount": d3.mean(filtered_data,function(d) { return d.mean_debt_graduated; })},
                 {"category": "national", "amount": national_avgs.mean_debt}
           ]},
-          {"name": "mean price", "values": [
+          {"name": "Net price", "values": [
                 {"category": "school", "amount": selectedSchool.mean_price},
                 {"category": "state", "amount": d3.mean(filtered_data,function(d) { return d.mean_price; })},
                 {"category": "national", "amount": national_avgs.mean_price}
           ]},
-          {"name": "grad rate", "values": [
+          {"name": "Graduation rate", "values": [
                 {"category": "school", "amount": selectedSchool.completion_rate},
                 {"category": "state", "amount": d3.mean(filtered_data,function(d) { return d.completion_rate; })},
                 {"category": "national", "amount": national_avgs.completion_rate}
           ]},
-          {"name": "repay rate", "values": [
+          {"name": "Repayment rate", "values": [
                 {"category": "school", "amount": selectedSchool.repayment_rate},
                 {"category": "state", "amount": d3.mean(filtered_data,function(d) { return d.repayment_rate; })},
                 {"category": "national", "amount": national_avgs.repayment_rate}
           ]}
         ];
-        console.log(bar_groups);
 
         // margins
-        var margin = {top: 0, right: 0, bottom: 0, left: 0},
-            width = 300 - margin.left - margin.right,
-            height = 100 - margin.top - margin.bottom;
+        var margin = {top: 30, right: 0, bottom: 0, left: 0},
+            width = 800 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
 
         // create responsive svg
         this.svg2 = d3v4.select("#schoolinfo").selectAll("svg")
           .data(bar_groups)
           .enter()
-              .append("div")
-                .classed("col-xs-6", true)
-                .append("div")
-                .classed("svg-container-bars", true) //container class to make it responsive
-                .append("svg:svg")
-                //responsive SVG needs these 2 attributes and no width and height attr
-                .attr("preserveAspectRatio", "xMinYMin meet")
-                .attr("viewBox", "0 0 800 750")
-                //class to make it responsive
-                .classed("svg-content-responsive", true)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .append("div")
+              .classed("col-xs-6", true)
+            .append("div")
+              .classed("svg-container-bars", true) //container class to make it responsive
+            .append("svg:svg")
+              //responsive SVG needs these 2 attributes and no width and height attr
+              .attr("preserveAspectRatio", "xMinYMin meet")
+              .attr("viewBox", "0 0 800 400")
+              //class to make it responsive
+              .classed("svg-content-responsive", true)
+              .attr("id", function(d) { return d.name })
+            .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+           .each(function(d, i){ // creates every bar chart separately
+                var svg = d3.select(this);
+                var allValues = d.values;
+                var xMax = d3v4.max(allValues, function(d) { return d.amount; })
 
-        // define scales
-        var barX = d3v4.scaleLinear()
-                    .range([0,width]).nice();
-        var barY = d3v4.scalePoint()
-                    .range([height,0])
-                    .domain(["state", "school", "national"]);
+                // define scales
+                var barX = d3v4.scaleLinear()
+                            .range([0,width]).nice()
+                            .domain([0, xMax]);
+                var barY = d3v4.scaleBand()
+                            .range([height,0])
+                            .padding(0.3)
+                            .domain(["national", "state", "school"]);
 
-        var xAxis = d3.svg.axis()
-            .scale(barX)
-            .orient("bottom");
+                            svg.append("g")
+                            .append("text")
+                            .attr("x", 0)
+                            .attr("y", 0)
+                            .attr("dy", ".71em")
+                            .attr("text-anchor", "start")
+                            .attr("font-size", "6rem")
+                            .style("text-transform", "uppercase")
+                            .text(function(d) { return d.name});
 
-        var yAxis = d3.svg.axis()
-            .scale(barY)
-            .orient("left");
 
-        var g2 = this.svg2.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        g2.append("g")
-          .attr("class", "axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis)
-          .selectAll("text")
-            .attr("y", -5)
-            .attr("x", -10)
-            .attr("transform", "rotate(-90)")
-            .style("text-anchor", "end");
+                var g2 = svg.selectAll("#bar")
+                  .data(allValues)
+                  .enter().append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        g2.append("g")
-            .attr("class", "axis")
-            .attr("stroke-width", 0)
-            .call(yAxis);
+                g2.append("rect")
+                    .attr("x", margin.left )
+                    .attr("height", function(d) { return barY.bandwidth() })
+                    .attr("y", function(d){ return barY(d.category) + margin.top; })
+                    .attr("width", function(d) { return barX(d.amount); })
+                    .attr("class", function(d) { return d.category })
+                    .attr("id", "bar");
 
-        this.svg2.selectAll("bar")
-            .data(function(d) { return d.values; })
-          .enter().append("rect")
-            .attr("x", margin.left )
-            .attr("height", 10)
-            .attr("y", function(d){ return barY(d.category); })
-            .attr("width", function(d) { return barX(d.amount); });
-
+                g2.append("text")
+                  .text(function(d) { return d.amount.toFixed(2); })
+                  .attr("y", function(d){ return barY(d.category) + margin.top + barY.bandwidth()/1.4; })
+                  .attr("x", function(d){ return (barX(d.amount)) - 20; })
+                  .attr("text-anchor", "middle")
+                  .attr("fill", "#fff")
+                  .style("text-anchor", "end")
+                  .style("font-size", "4rem");
+        })
     } //closes bar chart
+
+    // highlights circle and creates info box on dropdown selection
+    function dropClick(d) {
+        // gets value of the selected option
+        var selectedValue = d3.event.target.value;
+
+        filtered_data.forEach(function (d) {
+            // loop through json data to match td entry
+            // removes info box if show all is selected
+            if (selectedValue == "show-all") {
+                $('#schoolinfo').html('');
+            } else if (selectedValue === d.college) {
+                //for each data object in the features array (d), compare it's
+                //name against the one you got from the event object
+
+                // pass data element to scatterHover function so the dropdown
+                // responds the same way clicking on a circle does
+                buildBarCharts(d);
+            }
+        });
+
+        // increases dot size and changes color on dropdown click
+        d3.selectAll('.dot')
+            .attr('r', function(d, i) {
+                if (d.college == selectedValue) {
+                    return 15;
+                } else {
+                    return 10;
+                }
+            })
+            .style('fill', function(d, i) {
+                if (d.college == selectedValue) {
+                    return "#47b4f2";
+                } else {
+                    return "#d2d9da";
+                }
+            }
+        );
+    }
 
 
 };
 
-
-
-
-/* Helper functions */
-function scatterHover(d) {
-    details(d);
-    $('.dot').css('fill', '#d2d9da').attr("r", 10);
-    $(this).css('fill', "#1f3f48").attr("r", 15);
-    // buildBarCharts(d, state_data, national_data);
-}
-
-// highlights circle and creates info box on dropdown selection
-function dropClick(d) {
-    // gets value of the selected option
-    var selectedValue = d3.event.target.value;
-
-    filtered_data.forEach(function (d) {
-        // loop through json data to match td entry
-        // removes info box if show all is selected
-        if (selectedValue == "show-all") {
-            $('#schoolinfo').html('');
-        } else if (selectedValue === d.college) {
-            //for each data object in the features array (d), compare it's
-            //name against the one you got from the event object
-
-            // pass data element to scatterHover function so the dropdown
-            // responds the same way clicking on a circle does
-            scatterHover(d);
-        }
-    });
-
-    // increases dot size and changes color on dropdown click
-    d3.selectAll('.dot')
-        .attr('r', function(d, i) {
-            if (d.college == selectedValue) {
-                return 15;
-            } else {
-                return 10;
-            }
-        })
-        .style('fill', function(d, i) {
-            if (d.college == selectedValue) {
-                return "#1f3f48";
-            } else {
-                return "#47b4f2";
-            }
-        }
-    );
-}
-
-// function to populate the info box
-function details (d) {
-    var details = "<h3>" + d.college + "</h3><span class='category'>Median earnings:</span> $";
-    details += d.median_earnings.toLocaleString() + "</br><span class='category'>Average debt:</span> $";
-    details += d.mean_debt_graduated.toLocaleString() + "</br><span class='category'>Average net price:</span> $";
-    details += d.mean_price.toLocaleString() + "</br><span class='category'>Graduation rate:</span> ";
-    details += (d.completion_rate*100).toFixed(2) + "%</br><span class='category'>Repayment rate:</span> ";
-    details += (d.repayment_rate*100).toFixed(2) + "%";
-    $('#schoolinfo').html(details);
-}
 
 // Add a regression line
 function updateRegression(schools) {
